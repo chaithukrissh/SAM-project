ca3cb30e9d48f95fdd9771beca44a5a478b59671
chaithukrissh <chaithanya.thaticherla@gmail.com>
2023-12-06 11:48:48 +0530

    this is sam-project

---
+++ design.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
+design = Flask(__name__)
+lambda_function_endpoint = 'https://g5h3ff4t34.execute-api.ap-northeast-1.amazonaws.com/default/all-methods-function'
+
+@design.route('/')
+def home():
+    return render_template('home.html')
+
+@design.route('/add')
+def index():
+    return render_template('add.html')
+
+
+@design.route('/get')
+def get():
+    return render_template('get.html')
+
+@design.route('/delete')
+def delete():
+    return render_template('delete.html')
+
+
+@design.route('/get_data', methods=['POST'] )
+def submit_post():
+    user_name = request.form.get('user_name')
+    
+    data = {
+	"user_name":user_name
+    }
+
+    data1=json.dumps(data)
+
+
+    headers = {
+    "Content-Type": "application/json"
+    }
+    
+    
+    response = requests.get(lambda_function_endpoint, data=data1 ,headers=headers )
+
+    if response.status_code == 200:
+        prettified_data = json.dumps(response.text, indent=2)
+        return render_template('get_response.html', result=json.loads(prettified_data))
+    else:
+        prettified_data = json.dumps(response.text, indent=2)
+        return render_template('get_response.html', result=json.loads(prettified_data))
+
+
+
+@design.route('/add_data', methods=['POST' , 'GET'])
+def submit_add():
+    
+    user_name = request.form.get('user_name')
+    email = request.form.get('email')
+    data = {
+	"user_name":user_name,
+	"email": email
+    }
+
+    data1=json.dumps(data)
+
+
+    headers = {
+    "Content-Type": "application/json"
+    }
+    
+    response = requests.post(lambda_function_endpoint, data=data1 ,headers=headers )
+    
+    
+    if response.status_code == 200:
+        success_message = "User registered successfully!"
+        return render_template('add.html', message=success_message, message_type='success')
+    else:
+        return jsonify({'status': 'error', 'message': 'Error occurred while registering the user.'})
+
+
+
+
+
+@design.route('/delete_data', methods=['POST' , 'GET'])
+def submit_delete():
+    # Get data from the form
+    user_name = request.form.get('user_name')
+    email = request.form.get('email')
+
+    data = {
+	"user_name":user_name,
+	"email": email
+    }
+
+    data1=json.dumps(data)
+
+    headers = {
+    "Content-Type": "application/json"
+    }
+
+    response = requests.delete(lambda_function_endpoint, data=data1 ,headers=headers )
+    
+
+    if response.status_code == 200:
+        success_message = "User Deleted successfully!"
+        return render_template('delete.html', message=success_message, message_type='success')
+    else:
+        return jsonify({'status': 'error', 'message': 'Error occurred while deleting the user.'})
+
+
+
+if __name__ == '__main__':
+    design.run(debug=True)
---
+++ test_function.py
@@ -0,0 +1,58 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from get_items import lambda_handler
+import json
+
+class TestLambdaFunction(unittest.TestCase):
+
+    @patch('boto3.resource')
+    def test_post_method(self, mock_resource):
+        mock_dynamodb = MagicMock()
+        mock_resource.return_value = mock_dynamodb
+        event = {
+            'httpMethod': 'POST',
+            'body': '{"user_name": "test", "email": "test@gmail.com"}'
+        }
+        # mock_dynamodb.Table.return_value.get_item.return_value = {}
+        response = lambda_handler(event, None)
+
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(response['body'], '"User registered successfully!"')
+
+    @patch('boto3.resource')
+    def test_get_method(self, mock_resource):
+        mock_dynamodb = MagicMock()
+        mock_resource.return_value = mock_dynamodb
+        # mock_table = mock_dynamodb.Table.return_value
+        event = {
+            'httpMethod': 'GET',
+            'body': '{"user_name": "test123"}'
+        }
+        mock_dynamodb.Table.return_value.query.return_value = {}
+
+        
+        response = lambda_handler(event, None)
+
+        self.assertEqual(response['statusCode'], 200)
+        
+        parsed_body = json.loads(response['body'])
+        
+        self.assertEqual(parsed_body, [{"user_name": "test123", "email": "test123@gmail.com"}])
+
+    @patch('boto3.resource')
+    def test_delete_method(self, mock_resource):
+        mock_dynamodb = MagicMock()
+        mock_resource.return_value = mock_dynamodb
+        event = {
+            'httpMethod': 'DELETE',
+            'body': '{"user_name": "test", "email": "test@gmail.com"}'
+        }
+        mock_dynamodb.Table.return_value.delete_item.return_value = {}
+
+        response = lambda_handler(event, None)
+
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(response['body'], '"User deleted successfully!"')
+
+if __name__ == '__main__':
+    unittest.main()
